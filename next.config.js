4c0abbece50d40b14356cd0a5096530d19b0089c
Ertan Kayalar <ertankayalar@gmail.com>
2020-09-06 17:00:46 +0300

    sitemap feature

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 const withMDX = require('@next/mdx')()
 module.exports = withMDX({
     pageExtensions: ['md', 'mdx', 'jsx', 'js', 'ts', 'tsx'],
+
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require('./lib/generateSiteMap')
+      }
+  
+      return config
+    }
   })
 
