cc9b1d76adadfed02098ca86b608dc2d4f0fa649
Ertan Kayalar <ertankayalar@gmail.com>
2020-12-12 16:17:43 +0300

    testfs fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
-import testfun from '../lib/testfs'
 
 export default function Home() {
   return (
