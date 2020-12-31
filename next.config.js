7956820b56a15b0c3509bd775639a5997abbd470
Ertan Kayalar <ertankayalar@gmail.com>
2020-12-31 17:47:07 +0300

    config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
+const { getRedirectStatus } = require('next/dist/lib/load-custom-routes')
+
 const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
+  async redirects() {
+    return [
+      {
+        source: '/yazilim-uzmani-olmak',
+        destination: 'https://ertankayalar.com.tr/yazilim-uzmani-olmak',
+        permanent: true,
+      },
+      {
+        source: '/iletisim',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/dijital-pazarlama',
+        destination: 'https://pazarlama.info.tr',
+        permanent: true,
+      },
+      {
+        source: '/sozluk',
+        destination: 'https://pazarlama.info.tr/sozluk',
+        permanent: true,
+      },
+      {
+        source: '/sozluk-bolumu/dijital-pazarlama-sozlugu',
+        destination: 'https://pazarlama.info.tr/sozluk',
+        permanent: true,
+      },
+    ]
+  },
   env: {
     locale: 'en',
     siteName: 'ertankayalar.com',
